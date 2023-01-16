a31dd507891671d6a41c4403f60b90238dba4c03
omjustnikova <omjustnikova@gmail.com>
2023-01-16 21:31:11 +0600

    Some changes after https://github.com/Ziyakaev code review

--- src/main/java/ru/hh/school/dao/EmployerDao.java
+++ src/main/java/ru/hh/school/dao/EmployerDao.java
@@ -22,7 +22,7 @@ public class EmployerDao extends GenericDao {
   public Employer getEager(int employerId) {
     return getSession()
         .createQuery("from Employer employer " +
-                        "join fetch employer.vacancies " +
+                        "left join fetch employer.vacancies " +
                         "where employer.id = :employerId", Employer.class)
         .setParameter("employerId", employerId)
         .getSingleResult();
